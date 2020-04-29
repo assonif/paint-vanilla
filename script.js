2fcc88a51f07fb7ba149be09e54023a45710413a
Fabio Furtado <fabioassoni06@gmail.com>
2020-04-29 13:13:31 -0300

    borracha + reload + cursor

--- script.js
+++ script.js
@@ -10,13 +10,15 @@ document.addEventListener('DOMContentLoaded', () => {
     prevPos: null,
   };
 
+  var lineWidth = 5;
+
   const paintable = document.querySelector('#paintable-area');
   const context = paintable.getContext('2d');
 
   paintable.width = 700;
   paintable.height = 500;
 
-  context.lineWidth = 5;
+  context.lineWidth = lineWidth;
 
   const drawLine = (line) => {
     
@@ -57,39 +59,57 @@ document.addEventListener('DOMContentLoaded', () => {
   const plus = document.querySelector("#more");
 
   plus.addEventListener("click", () => {
-    context.lineWidth += 1;
+    lineWidth += 4;
+    context.lineWidth = lineWidth;
   });
 
 
   const less = document.querySelector("#less");
 
   less.addEventListener("click", () => {
-    context.lineWidth -= 1;
+    lineWidth -= 4;
+    context.lineWidth = lineWidth;
   });
   
 
   const red = document.querySelector(".red");
   red.addEventListener("click", () => {
+    context.lineWidth = lineWidth
     context.strokeStyle = "red";
   });
 
   const blue = document.querySelector(".blue");
   blue.addEventListener("click", () => {
+    context.lineWidth = lineWidth
     context.strokeStyle = "blue";
   });
 
   const green = document.querySelector(".green");
   green.addEventListener("click", () => {
+    context.lineWidth = lineWidth
     context.strokeStyle = "green";
   });
 
   const yellow = document.querySelector(".yellow");
   yellow.addEventListener("click", () => {
+    context.lineWidth = lineWidth
     context.strokeStyle = "yellow";
   });
 
   const black = document.querySelector(".black");
   black.addEventListener("click", () => {
+    context.lineWidth = lineWidth
     context.strokeStyle = "black";
+  });
+
+  const eraser = document.querySelector(".eraser");
+  eraser.addEventListener("click", () => {
+    context.strokeStyle = "white";
+    context.lineWidth = 16;
+  });
+
+  const reload = document.querySelector(".reload");
+  reload.addEventListener("click", () => {
+    window.location.reload();
   });
 })
